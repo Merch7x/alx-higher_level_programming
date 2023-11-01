fca12412d17b0cd445120d6cc5f90c033f55648b
Merch7x <aquamaingi@gmail.com>
2023-11-01 05:34:26 +0300

    Update condutional

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new_str = ""
-    i = 0
-    for c in str:
-        if i != n:
-            new_str += c
-            i += 1
-    return new_str
+    if n >= 0:
+        return (str[:n] + str[n + 1:])
+    return (str)
