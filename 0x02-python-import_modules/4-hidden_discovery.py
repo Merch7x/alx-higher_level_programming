9851dcf5e01076b67fa3fd880904953941b4dc39
Merch7x <aquamaingi@gmail.com>
2023-11-02 13:13:08 +0300

    Revert import statement

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from re import match
+import hidden_4
 
 if __name__ == "__main__":
-    import hidden_4
     original = dir(hidden_4)
     new = [item for item in original if not match(r'^__', item)]
     print("{}\n{}\n{}".format(*new))
