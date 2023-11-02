897a4fcaecb502a70b17eb1301e96db1012ce672
Merch7x <aquamaingi@gmail.com>
2023-11-02 22:13:56 +0300

    Use dict instead of match case

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,19 +3,19 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 
-def operator_to_use(operator):
-    match operator:
-        case '+':
-            return add
-        case '-':
-            return sub
-        case '*':
-            return mul
-        case '/':
-            return div
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+# def operator_to_use(operator):
+#     match operator:
+#         case '+':
+#             return add
+#         case '-':
+#             return sub
+#         case '*':
+#             return mul
+#         case '/':
+#             return div
+#         case _:
+#             print("Unknown operator. Available operators: +, -, * and /")
+#             sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -23,8 +23,12 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    operator = operator_to_use(sys.argv[2])
-    result = operator(a, b)
-    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
+    print("{} {} {} = {}".format(
+        a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
