5a1a72b8018ff94f65ce10213597fbe1d300c79d
Merch7x <aquamaingi@gmail.com>
2023-11-02 12:47:30 +0300

    Use dir() function

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from re import match
+from hidden_4 *
+
+if __name__ == "__main__":
+    original = dir(hidden_4)
+    new = [item for item in original if not match(r'^__', item)]
+    print("{}\n{}\n{}".format(*new))
+    
