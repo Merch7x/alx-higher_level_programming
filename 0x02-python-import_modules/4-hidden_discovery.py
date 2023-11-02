26a519882304f182cbb449be57ccd23454d5eb60
Merch7x <aquamaingi@gmail.com>
2023-11-02 13:18:02 +0300

    Use for loop instead of sequence unpacking

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,4 +5,5 @@ import hidden_4
 if __name__ == "__main__":
     original = dir(hidden_4)
     new = [item for item in original if not match(r'^__', item)]
-    print("{}\n{}\n{}".format(*new))
+    for name in new:
+        print(name)
