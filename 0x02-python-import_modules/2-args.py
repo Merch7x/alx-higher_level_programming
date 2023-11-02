e124ab608b2fb47297a974fb99628bb48ab3cb2e
Merch7x <aquamaingi@gmail.com>
2023-11-02 12:12:36 +0300

    Update name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 
-argument = "arguments" if len(argv) == 1 or len(argv) > 2 else "argument"
-if len(argv) == 1:
-    print("0 {}.".format(argument))
-elif len(argv) >= 2:
-    print("{} {}:".format(len(argv) - 1, argument))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    argument = "arguments" if len(argv) == 1 or len(argv) > 2 else "argument"
+    if len(argv) == 1:
+        print("0 {}.".format(argument))
+    elif len(argv) >= 2:
+        print("{} {}:".format(len(argv) - 1, argument))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
