765c02a6485f1f2722e96375bf9059e13e9dd22c
Merch7x <aquamaingi@gmail.com>
2023-11-06 13:33:29 +0300

    Add else block

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0 | idx >= len(my_list):
         return None
-    # for x in my_list:
-    #     if x == my_list[idx]:
-    #         return x
+    else:
+        for x in my_list:
+            if x == my_list[idx]:
+                return x
