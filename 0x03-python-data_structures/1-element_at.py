166f5df7b1f02fb019481091ee5bedec7222a763
Merch7x <aquamaingi@gmail.com>
2023-11-06 16:12:04 +0300

    Imply else

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,4 @@
 def element_at(my_list, idx):
     if idx < 0 | idx > len(my_list) - 1:
         return None
-    for x in my_list:
-        if x == my_list[idx]:
-            return my_list[idx]
+    return my_list[idx]
