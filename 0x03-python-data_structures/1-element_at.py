5c9761e98c088767c83e23e316ae3978542baed9
Merch7x <aquamaingi@gmail.com>
2023-11-06 16:10:51 +0300

    Fix idx check

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 | idx >= len(my_list):
+    if idx < 0 | idx > len(my_list) - 1:
         return None
     for x in my_list:
         if x == my_list[idx]:
