af4f3017e88186575744b3079dd3cacbd059064e
Merch7x <aquamaingi@gmail.com>
2023-11-06 14:34:10 +0300

    Access element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0 | idx >= len(my_list):
         return None
-    else:
-        for x in my_list:
-            if x == my_list[idx]:
-                return x
+    for x in my_list:
+        if x == my_list[idx]:
+            return x
