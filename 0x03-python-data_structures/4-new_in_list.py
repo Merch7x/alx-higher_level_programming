5cc9d6d3dea2e84923f6d6ab23b46565de164eea
Merch7x <aquamaingi@gmail.com>
2023-11-06 14:48:26 +0300

    Replace in copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    for x in my_list:
+        if x == my_list[idx]:
+            my_list.remove(x)
+            my_list.insert(idx, element)
+            return my_list
