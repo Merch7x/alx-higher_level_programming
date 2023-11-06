bff398f0a09dc9072952e25dc8a948a4cfbb8998
Merch7x <aquamaingi@gmail.com>
2023-11-06 16:32:05 +0300

    Return a copy of alist

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    for x in my_list:
-        if x == my_list[idx]:
-            my_list.remove(x)
-            my_list.insert(idx, element)
-            return my_list
+        return new_list
+    for x in new_list:
+        if x == new_list[idx]:
+            new_list.remove(x)
+            new_list.insert(idx, element)
+            return new_list
