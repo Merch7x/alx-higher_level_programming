3e4ca9c25cc29f4269f474993fb5fa398fad3ae4
Merch7x <aquamaingi@gmail.com>
2023-11-08 21:32:11 +0300

    Add check for an empty dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     best = 0
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     best = max(a_dictionary.values())
     for key, value in a_dictionary.items():
