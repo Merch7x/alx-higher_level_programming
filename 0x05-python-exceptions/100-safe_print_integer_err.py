ddb1b47553d6a43d2fbd71e8f90bcfc1098137cb
Merch7x <aquamaingi@gmail.com>
2023-11-20 23:32:25 +0300

    Refactor code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,8 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError) as e:
-        sys.stderr.write("Exception: {}".format(e))
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
+    else:
+        return True
