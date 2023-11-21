f581c59f204093dbcf77594598e4667495a56429
Merch7x <aquamaingi@gmail.com>
2023-11-21 21:00:09 +0300

    Add validation to init constructor

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Defines a class square """
+
+
+class Square:
+    """ Defines a class square """
+    def __init__(self, size=0):
+        """ __init__ constructor docstring
+        Args:
+            size: Size of the square.
+            """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+
+
