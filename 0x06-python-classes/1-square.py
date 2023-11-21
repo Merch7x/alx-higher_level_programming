a022e8ffeef7670e5cd55cc377a187d2619993ed
Merch7x <aquamaingi@gmail.com>
2023-11-21 20:28:55 +0300

    Declaring an init constructor

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Defines a class square """
+
+
+class Square:
+    """ Defines a class square """
+    def __init__(self, size):
+        """ __init__ constructor docstring
+        Args:
+            size: Size of the square.
+            """
+        self.__size = size
