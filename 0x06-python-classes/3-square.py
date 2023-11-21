7b8a5941226dd35d1c3e03cc0ac8094a764acca5
Merch7x <aquamaingi@gmail.com>
2023-11-21 21:21:34 +0300

    Add methods to a class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Defines a class square """
+
+
+class Square:
+    """ Defines a class square """
+    def __init__(self, size=0):
+        """ __init__ constructor docstring
+        Args:
+            size: Size of the square.
+            """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """ determines the area of a square"""
+        result = self.__size * self.__size
+        return result
