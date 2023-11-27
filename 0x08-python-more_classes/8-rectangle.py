f0d35c1c7aa5952b071045169cf4657f1f25bba5
Merch7x <aquamaingi@gmail.com>
2023-11-27 22:55:40 +0300

    Refactor

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -24,16 +24,15 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """Evaluates instances by area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_1.area() == rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        elif rect_1.area() > rect_2.area():
-            return rect_1
-        elif rect_2.area() > rect_1.area():
+        else:
             return rect_2
 
     @property
