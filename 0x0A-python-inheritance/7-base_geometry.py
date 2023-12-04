90f820d4e3e47af691c92c9c4f2e4ef13087e792
Merch7x <aquamaingi@gmail.com>
2023-12-05 01:26:54 +0300

    Add integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Creates an empty class"""
+
+
+class BaseGeometry:
+    """ An empty class"""
+    def area(self):
+        """A public method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value
+
+        Raises:
+            TypeError: if value is not an int
+            ValueError: if value is less than or equal to zero
+        """
+        self.name = name
+        if not isinstance(value, int):
+            raise TypeError(f"{self.name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+        self.value = value
