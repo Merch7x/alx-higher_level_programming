26ff0da05325c58b94e789b3f11d3afd7d67be00
Merch7x <aquamaingi@gmail.com>
2023-12-05 03:03:06 +0300

    Fix return value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,9 +15,8 @@ class BaseGeometry:
             TypeError: if value is not an int
             ValueError: if value is less than or equal to zero
         """
-        self.name = name
         if not isinstance(value, int):
-            raise TypeError(f"{self.name} must be an integer")
+            raise TypeError(f"{name} must be an integer")
         elif value <= 0:
-            raise ValueError(f"{self.name} must be greater than 0")
-        return value
+            raise ValueError(f"{name} must be greater than 0")
+
