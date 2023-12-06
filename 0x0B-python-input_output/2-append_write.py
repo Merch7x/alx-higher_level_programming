e5110aa6349d9fbde703c20f82cd25563160424c
Merch7x <aquamaingi@gmail.com>
2023-12-06 23:07:22 +0300

    Append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Append to file"""
+
+
+def append_write(filename="", text=""):
+    """append to a file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        data = f.write(text)
+        return(data)
