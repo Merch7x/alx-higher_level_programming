eec1844f46bca8101bbcefa2fba5ed15b1fb21e9
Merch7x <aquamaingi@gmail.com>
2023-12-07 02:31:31 +0300

    Use json load

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Deserializes json from a file to Python object"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Retrieves json from a file and deserializes it
+    load stands for load from file
+    """
+    with open(filename, 'r') as f:
+        json.load(f)
