10a1bfad314d65608e6c14fbf519bd6cfe84b458
Merch7x <aquamaingi@gmail.com>
2023-12-06 23:09:29 +0300

    fix linter issues

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def append_write(filename="", text=""):
     """append to a file"""
     with open(filename, 'a', encoding='utf-8') as f:
         data = f.write(text)
-        return(data)
+        return data
