ef34a8787eaad62c39f7623741eff53298961432
Merch7x <aquamaingi@gmail.com>
2023-12-14 21:51:09 +0300

    Fix linter errors

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,17 +3,17 @@
 
 
 class Base():
-  """A base class"""
-  __nb_objects = 0
-  def __init__(self, id=None):
-    """Instanciate a class object
-    
-    Args:
-      id: a form of object id
-    """
-    if id is None:
-      Base.__nb_objects += 1
-      self.id = Base.__nb_objects
-    else:
-      self.id = id
+    """A base class"""
+    __nb_objects = 0
 
+    def __init__(self, id=None):
+        """Instanciate a class object
+
+        Args:
+            id: a form of object id
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
