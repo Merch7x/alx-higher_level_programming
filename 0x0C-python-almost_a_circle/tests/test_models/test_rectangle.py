34e665511a627c1d5032a993d7ff4f3b790958bd
Merch7x <aquamaingi@gmail.com>
2023-12-14 23:38:14 +0300

    Add tests for rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,3 +18,33 @@ class TestRectangle(unittest.TestCase):
         obj = Rectangle(height=15, width=9, x=7, y=9, id=None)
         self.assertIsInstance(obj, Rectangle)
 
+    def test_typeError(self):
+        """Test type of the attributes"""
+        with self.assertRaises(TypeError):
+            obj = Rectangle(height="hi", width=7)
+        with self.assertRaises(TypeError):
+            obj = Rectangle(height=8, width="jik")
+        with self.assertRaises(TypeError):
+            obj = Rectangle(height=15, width=10, x="it", y=2)
+        with self.assertRaises(TypeError):
+            obj = Rectangle(height=20, width=10, x=5, y="it")
+    
+    def test_valueError_width_height(self):
+        """Test value of width and height"""
+        with self.assertRaises(ValueError):
+            obj = Rectangle(height=0, width=5)
+        with self.assertRaises(ValueError):
+            obj = Rectangle(height=20, width=0)
+        with self.assertRaises(ValueError):
+            obj = Rectangle(height=-5, width=10)
+        with self.assertRaises(ValueError):
+            obj = Rectangle(height=20, width=-6)
+    
+    def test_x_y_valueError(self):
+        """Test value of x and y"""
+        with self.assertRaises(ValueError):
+            obj = Rectangle(height=20, width=10, x=-1, y=6)
+        with self.assertRaises(ValueError):
+            obj = Rectangle(height=20, width=10, x=7, y=-4)
+
+
