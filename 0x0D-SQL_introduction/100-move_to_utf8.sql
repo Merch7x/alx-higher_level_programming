dffe7a3410cb48cc50689ef16b4540097d8fbb8c
Merch7x <aquamaingi@gmail.com>
2024-01-17 14:04:53 +0300

    Chnage database charaterset

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+-- set the default character set and collation for the database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- convert table to new character set
+AlTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+ALTER TABLE first_table
+MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE
+utf8mb4_unicode_ci;
