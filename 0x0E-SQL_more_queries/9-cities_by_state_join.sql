26cf86e678b570717026a1fffe379c187acc488f
Merch7x <aquamaingi@gmail.com>
2024-01-18 01:19:43 +0300

    Fix spelling error

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in hbtn_0d_usa
-SELECT citites.id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON citites.state_id = states.id;
+JOIN states ON cities.state_id = states.id;
