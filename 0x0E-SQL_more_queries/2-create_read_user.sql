25a15a552d2338e4f74354b36151bd413d17273d
Merch7x <aquamaingi@gmail.com>
2024-01-17 21:38:18 +0300

    Rectify user creation

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- create a database and a user
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
 Use `hbtn_0d_2`;
 
-CREATE USER 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
