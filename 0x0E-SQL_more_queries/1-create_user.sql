794c13a1d9e06784a52a7476a3baf0c17ebd7e9d
Merch7x <aquamaingi@gmail.com>
2024-01-17 21:27:19 +0300

    Use quotes for string literals

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- create mysql user
-CREATE USER `user_0d_1`@`localhost` IF NOT EXISTS IDENTIFIED BY `user_0d_1_pwd`;
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT SELECT
+GRANT ALL PRIVILEGES
 ON *.*
-TO `user_0d_1`@`localhost`;
+TO 'user_0d_1'@'localhost';
