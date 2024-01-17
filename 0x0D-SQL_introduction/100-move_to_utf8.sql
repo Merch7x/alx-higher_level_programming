d2553d4730d38716bd27d8bbf45aaf0c60b270e5
Merch7x <aquamaingi@gmail.com>
2024-01-17 14:09:40 +0300

    Update db name

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,10 +2,10 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- convert table to new character set
-AlTER TABLE first_table
+AlTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE
 utf8mb4_unicode_ci;
