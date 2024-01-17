90c2a0ae4ffd60636a1c9a65e3bd46256445fdd2
Merch7x <aquamaingi@gmail.com>
2024-01-17 14:24:31 +0300

    Set db to use

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,14 @@
--- set the default character set and collation for the database
+-- set the db to use
 USE `hbtn_0c_0`
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode;
+
+-- Convert the hbtn_0c_0 database to UTF-8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the first_table table to UTF-8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the name field in first_table to UTF-8
+ALTER TABLE first_table
+MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
