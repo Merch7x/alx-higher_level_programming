e69ef6311fd915dfb40e7c906c9439cf012250fb
Merch7x <aquamaingi@gmail.com>
2024-01-17 15:07:21 +0300

    Change query structure

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,5 @@
 -- disaplys average temperature
-SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY 'avg_temp' DESC;
+SELECT city, AVG(value) as 'avg_temp'
+FROM temperatures
+GROUP BY city 
+ORDER BY 'avg_temp' DESC;
