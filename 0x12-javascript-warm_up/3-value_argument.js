3d5c1b649aec0d38ec3145e9e7d53e40796a3f75
Merch7x <aquamaingi@gmail.com>
2024-02-12 19:21:19 +0300

    Log args

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,8 @@ const argv = process.argv;
 
 if (!argv[2]) {
   console.log('No argument');
-} else {
-  for (const arg of argv.slice(2)) {
-    console.log(arg);
-  }
+}
+
+for (const arg of argv.slice(2)) {
+  console.log(arg);
 }
