e7de431c32449a6ff2bf1bd9fb7426dcb9180b6f
Merch7x <aquamaingi@gmail.com>
2024-02-12 18:51:09 +0300

    Access cmd args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+if (argv[3]) {
+  console.log('Arguments found');
+} else if (argv[2]) {
+  console.log('Argument found');
+} else if (!argv[2]) {
+  console.log('No argument');
+}
