b39afe3cb63953a87909edfe4d6cd2a332d89d5d
Merch7x <aquamaingi@gmail.com>
2024-02-12 19:11:02 +0300

    Use for of loop

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const argv = process.argv;
 if (!argv[2]) {
   console.log('No argument');
 } else {
-  for (let i = 2; i < process.argv.length; i++) {
-    console.log(argv[i]);
+  for (const arg of argv.slice(2)) {
+    console.log(arg);
   }
 }
