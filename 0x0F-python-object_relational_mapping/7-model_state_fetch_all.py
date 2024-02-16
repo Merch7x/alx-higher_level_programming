eb57ebe2ddbc29744e3271e8c957312aae13e84a
Merch7x <aquamaingi@gmail.com>
2024-02-16 23:24:34 +0300

    Fix indents

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,7 +4,6 @@
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
@@ -12,10 +11,10 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-with Session() as session:
-    states = session.query(State).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    with Session() as session:
+        states = session.query(State).all()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
