3e056c389390af23a34fd9cdb5760bed60876cfa
Merch7x <aquamaingi@gmail.com>
2024-02-17 00:57:26 +0300

    Add new state object

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" lists all State objects that contain the letter a
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        new_state = State(id=6, name="Louisiana")
+        session.add(new_state)
+
+        states = session.query(State).filter_by(name="Louisiana")
+
+        if states:
+            for state in states:
+                print("{}".format(state.id))
