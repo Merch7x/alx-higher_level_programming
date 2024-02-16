00e831d8e2ec20595a0190c5f9a516c87b3df5f9
Merch7x <aquamaingi@gmail.com>
2024-02-16 22:06:01 +0300

    Get all cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+takes in the name of a state as an argument
+and lists all cities of that state
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        print("Usage: <user> <passwd> <db> <state_name>")
+        sys.exit(1)
+
+    try:
+        db = MySQLdb.connect(
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
+        cur.execute(
+            "SELECT cities.name\
+                FROM cities\
+                    INNER JOIN states ON cities.state_id = states.id\
+                        WHERE states.name = '{}'".format(sys.argv[4]))
+        [print(city) for city in cur.fetchall()]
+    except MySQLdb.Error as e:
+        print("Error:", e)
+
+    finally:
+        cur.close()
+        db.close()
