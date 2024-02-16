b8a8038fb80beac3ab571297d6cdb44226bf5355
Merch7x <aquamaingi@gmail.com>
2024-02-16 17:40:39 +0300

    Add db connection to try block

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with 'N'
+Lists all states with a name provided by the user through cmd input
 from the databse hbtn_ 0e_0_usa
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
+
+    if len(sys.argv) != 5:
+        print("Usage: <user> <passwd> <db> <search_parameter>")
+        sys.exit(1)
 
     try:
+        db = MySQLdb.connect(
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
         cur.execute(
             "SELECT * FROM `states` where name = '{}'".format(sys.argv[4]))
         [print(state) for state in cur.fetchall()]
