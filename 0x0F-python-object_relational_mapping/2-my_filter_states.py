02cadc4b11de4cb3adf07a8ab07a0f413b60347c
Merch7x <aquamaingi@gmail.com>
2024-02-16 22:28:42 +0300

    Fix formatting

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
         db = MySQLdb.connect(
             user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
-        cur.execute(
-            "SELECT * FROM states where name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+        query = "SELECT * FROM states where name = '{}' ORDER BY id ASC".format(
+            sys.argv[4])
+        cur.execute(query)
         [print(state) for state in cur.fetchall()]
     except MySQLdb.Error as e:
         print("Error:", e)
