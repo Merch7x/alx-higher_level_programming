d50637a2b89bc16def945a2f07dc116ef282f632
Merch7x <aquamaingi@gmail.com>
2024-02-16 22:59:50 +0300

    Create db model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+""" Creates a class State"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+# from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql+mysqldb://tim:tim@localhost/hbtn_0e_6_usa')
+Base = declarative_base()
+
+
+class State(Base):
+    """Creates a State object
+      Inherits from the Base of sqlalchemy
+       """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
