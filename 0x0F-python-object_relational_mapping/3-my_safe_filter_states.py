6884a3d0242fdf9185cd71a92df2f69d6b4ca04a
Merch7x <aquamaingi@gmail.com>
2024-02-16 17:45:05 +0300

    Prevent sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name provided by the user through cmd input
+from the databse hbtn_ 0e_0_usa
+Add args check to prevent sql injection
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        print("Usage: <user> <passwd> <db> <search_parameter>")
+        sys.exit(1)
+
+    try:
+        db = MySQLdb.connect(
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
+        cur.execute(
+            "SELECT * FROM `states` where name = '{}'".format(sys.argv[4]))
+        [print(state) for state in cur.fetchall()]
+    except MySQLdb.Error as e:
+        print("Error:", e)
+
+    finally:
+        cur.close()
+        db.close()
