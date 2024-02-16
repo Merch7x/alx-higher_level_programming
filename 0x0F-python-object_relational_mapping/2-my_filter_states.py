22dbb2e5aa593c0f51397a3eb8913d77b4f491af
Merch7x <aquamaingi@gmail.com>
2024-02-16 17:14:47 +0300

    Filter states by user imput

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with 'N'
+from the databse hbtn_ 0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    try:
+        cur.execute(
+            "SELECT * FROM `states` where name = '{}'".format(sys.argv[4]))
+        [print(state) for state in cur.fetchall()]
+    except MySQLdb.Error as e:
+        print("Error:", e)
+
+    finally:
+        cur.close()
+        db.close()
