8a5a4dd6600aa233d2ebf7067d03007e29269520
Merch7x <aquamaingi@gmail.com>
2024-02-19 00:09:31 +0300

    list relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""hat prints all City objects from the database
+"""
+import sys
+from relationship_state import State
+from relationship_city import City, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        for state in session.query(State).order_by(State.id):
+            print("{}: {}".format(state.id, state.name))
+            for city in state.cities:
+                print("    {}: {}".format(city.id, city.name))
