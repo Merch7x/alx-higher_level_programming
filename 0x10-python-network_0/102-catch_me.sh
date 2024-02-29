c704056ce038670bd835dd52c19b453c413f15a1
Merch7x <aquamaingi@gmail.com>
2024-03-01 00:25:04 +0300

    Update script

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#print out only a status code
-curl -s 0:5000/catch_me
+#makes a request to loclhost on port 5000 respond with a message
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0:5000/catch_me
