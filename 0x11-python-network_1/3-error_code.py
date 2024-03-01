afdae2dda835d1d878d85cc340d0aa75f914297e
Merch7x <aquamaingi@gmail.com>
2024-03-01 16:38:27 +0300

    Improve exception

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,7 +4,7 @@ takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)
  """
 from urllib.request import Request, urlopen
-from urllib.error import URLError
+from urllib.error import HTTPError
 import sys
 
 
@@ -15,8 +15,5 @@ if __name__ == '__main__':
         with urlopen(req) as response:
             content = response.read().decode('utf-8')
             print(content)
-    except URLError as e:
-        if hasattr(e, 'reason'):
-            print(e.reason)
-        elif hasattr(e, 'code'):
-            print(f'Error code: {e.code}')
+    except HTTPError as e:
+        print(f'Error code: {e.code}')
