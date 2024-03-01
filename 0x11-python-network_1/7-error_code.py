2d01394538aea2fef187cc8b67b26c3142dde876
Merch7x <aquamaingi@gmail.com>
2024-03-01 17:19:21 +0300

    Check status code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)
+ """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    req = requests.get(sys.argv[1])
+    status = req.status_code
+    if status >= 400:
+        print(f'Error code: {status}')
