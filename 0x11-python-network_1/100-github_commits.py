02f1161f4241dc90712fd9a9db6c2a7bcf7a8da6
Merch7x <aquamaingi@gmail.com>
2024-03-01 20:48:04 +0300

    Get commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+ """
+from requests.auth import HTTPBasicAuth
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    req = requests.get(
+        f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits')
+    data = req.json()
+
+    for commit in data[-10:]:
+        print(f"{commit.get('sha')}: {commit.get('authors')}")
