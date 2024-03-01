a059d71e55b7db3a13103ebf35d85ebbd8179003
Merch7x <aquamaingi@gmail.com>
2024-03-01 17:11:52 +0300

    Send post request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter
+and displays the body of the response
+ """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    data = {'email': sys.argv[2]}
+    req = requests.post(sys.argv[1], data)
+
+    print(req.text)
