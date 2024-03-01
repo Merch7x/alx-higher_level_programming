a633b4b8c8ac7fa05b742491aec6607020835ad9
Merch7x <aquamaingi@gmail.com>
2024-03-01 16:29:28 +0300

    Update response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,12 +12,13 @@ import sys
 
 if __name__ == '__main__':
     value = {'email': sys.argv[2]}
-    data = urlencode(value).encode('ascii')
+    data = urlencode(value)
+    data = data.encode('ascii')
     req = Request(sys.argv[1], data)
 
     try:
         with urlopen(req) as response:
-            content = response.read.decode('utf-8')
+            content = response.read().decode('utf-8')
             print(content)
     except URLError as e:
         if hasattr(e, 'reason'):
