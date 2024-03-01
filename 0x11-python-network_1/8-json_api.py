a92cdeaa3fefe40a632214cc058e869cff0197ff
Merch7x <aquamaingi@gmail.com>
2024-03-01 19:31:32 +0300

    Search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+ """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    val = "" if len(sys.argv) == 1 else sys.argv[1]
+    value = {'q': val}
+    req = requests.post('http://0.0.0.0:5000/search_user', data=value)
+
+    try:
+        data = req.json()
+        if data == {}:
+            print("No result")
+        else:
+            print(f"[<{data.get('id')}>] <{data.get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
